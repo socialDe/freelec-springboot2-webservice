c022ec8a83e3faee2d3edf78a90cf37738ccf3a3
socialDe <jaeuk9407@gmail.com>
2021-05-14 17:03:04 +0900

    deploy.sh 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '${print $1}')
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
